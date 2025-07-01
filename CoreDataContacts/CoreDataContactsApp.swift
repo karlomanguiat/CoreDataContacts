fbcc25a80a3c97cec64d188d3224a0cfc47010b0
Glenn Manguiat <karlo.manguiat@gmail.com>
2025-07-01 14:18:00 +0800

    Initial Commit

---
+++ CoreDataContacts/CoreDataContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataContactsApp.swift
+//  CoreDataContacts
+//
+//  Created by Glenn Karlo Manguiat on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
